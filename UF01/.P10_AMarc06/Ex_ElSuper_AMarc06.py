61e2dd1d1ec1e387311960c4068bfd5e657fb9bd
Markis_ag <marc.alonsogaitan@gmail.com>
2024-01-19 10:23:24 +0100

    Update 0.00

--- UF01/.P10_AMarc06/Ex_ElSuper_AMarc06.py
+++
@@ -1,7 +0,0 @@
-from El_Super.app import menu
-
-if __name__ == "__main__":
-    option = ''
-    while option != 'q':
-        option = menu.printMenu()
-        menu.menuSelector(option)
--- UF01/.P11_AMarc06/Ex1_AMarc06.py
+++
@@ -1,7 +0,0 @@
-dict = {}
-sentence = input('Enter a sentence: ').lower()
-words = sentence
-dict = dict.fromkeys(words, 0)
-for word in words:
-    dict[word] += 1
-print(dict)
